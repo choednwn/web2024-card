172faffe4d5b332c32adac20c0f7858660cb7759
Justin <choednwn@icloud.com>
2024-12-19 13:55:47 +0900

    DONEONDONEONDONEONDONE

--- src/app/presentation/page.tsx
+++ src/app/presentation/page.tsx
@@ -1,26 +1,32 @@
 "use client";
 
+import { ArrowRightIcon } from "lucide-react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const PresentationPage = () => {
+  const router = useRouter();
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center gap-12 overflow-hidden">
+    <div
+      onClick={() => router.push("/")}
+      className="flex h-screen w-screen flex-col items-center justify-center gap-12 overflow-hidden"
+    >
       <h1 className="text-8xl font-bold duration-1000 animate-in fade-in slide-in-from-bottom-5">
         주제: 카드게임
       </h1>
-      <ul className="flex flex-row gap-6 text-2xl duration-500">
-        <li className="delay-[75ms]">한민주</li>
-        <li className="delay-[100ms]">진채민</li>
-        <li className="delay-[125ms]">김나현</li>
-        <li className="delay-[150ms]">최우주</li>
-      </ul>
+      <div className="flex flex-row gap-6 text-2xl duration-500 animate-in fade-in slide-in-from-bottom-5">
+        <span>한민주</span>
+        <span>진채민</span>
+        <span>김나현</span>
+        <span>최우주</span>
+      </div>
       <ul className="flex flex-row items-center justify-center gap-24 pt-16 text-2xl">
         <li>
           <Image
             src="/presentation/ReactJS.png"
             width={400}
             height={400}
-            className="size-32"
+            className="size-32 duration-1000 animate-in slide-in-from-bottom-5"
             alt="reactjs"
           />
         </li>
@@ -29,7 +35,7 @@ const PresentationPage = () => {
             src="/presentation/NextJS.png"
             width={500}
             height={400}
-            className="h-16 w-auto invert"
+            className="h-16 w-auto invert duration-1000 animate-in slide-in-from-bottom-5"
             alt="reactjs"
           />
         </li>
@@ -38,11 +44,17 @@ const PresentationPage = () => {
             src="/presentation/MySQL.png"
             width={600}
             height={400}
-            className="h-32 w-auto"
+            className="h-32 w-auto duration-1000 animate-in slide-in-from-bottom-5"
             alt="reactjs"
           />
         </li>
       </ul>
+      <div
+        onClick={() => router.push("/")}
+        className="cursor-pointer rounded-full border-2 border-white p-6 transition-all duration-300 hover:bg-orange-400/80 hover:shadow-lg hover:shadow-orange-400"
+      >
+        <ArrowRightIcon size={52} className="relative" />
+      </div>
     </div>
   );
 };
